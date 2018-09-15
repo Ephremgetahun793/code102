e0844d734600458fbca6694b671201ec9c59533c
ephremgetahun793 <ephremgetahun793@gmail.com>
2018-09-15 11:31:45 -0400

    adding js file

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+
+let box= "bottle";
+console.log(box);
+
+box ="phone";
+console.log(box);
+
+box ="wire";
+console.log(box);
+
+ let city= "Dire Dewa";
+
+ console.log(city);
+
+  city= "washington city";
+
+ console.log(city);
+
